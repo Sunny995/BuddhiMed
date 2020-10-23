d6846f0e03c0f7227659df2aff0d5954b9dc9773
sunny kumar <sunnymax95@gmail.com>
2020-10-23 21:44:10 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public rawData1 = [];
+  public stateData = [
+    { x: 'CNF', text: 'Confirmed', y: 4184 },
+    { x: 'DEC', text: 'Deceased', y: 57 },
+    { x: 'REC', text: 'Recovered', y: 3937 },
+    { x: 'TST', text: 'Tested', y: 78303 },];
+    public stateTableData = [];
+
+  constructor(public service: DataService) { }
+  ngOnInit(): void {
+   
+  }
   title = 'budhi-app';
+  public tableShow = false;
+
+  showTable(value: string): void {
+    if (value === 'Dashboard') {
+      this.tableShow = false;
+    } else {
+      this.tableShow = true;
+    }
+  }
+
+
+  setStateData(stateCode: string): void {
+    let arr = [];
+    let arr1 = [];
+    let arr2 = [];
+    let arr3 = [];
+    this.service.getdata2()
+      .subscribe(data => {
+
+        Object.keys(data).map(function (key) {
+          arr.push({ [key]: data[key] })
+          return arr;
+        });
+        arr.forEach(u => {
+          Object.keys(u).map(function (key) {
+            if (key === stateCode) {
+              arr1.push(u[key].total)
+              arr2.push(u[key].districts);
+            }
+
+            return arr1;
+          });
+        });
+        Object.keys(arr2[0]).map(function (key) {
+          arr3.push({ name: key, 
+            confirmed: arr2[0][key].total.confirmed ,
+             deceased: arr2[0][key].total.deceased,
+             recovered: arr2[0][key].total.recovered,
+             tested: arr2[0][key].total.tested
+            })
+          return arr;
+        });
+        this.stateTableData = Object.assign([], arr3);
+        this.stateData = [
+          { x: 'CNF', text: 'Confirmed', y: arr1[0].confirmed },
+          { x: 'DEC', text: 'Deceased', y:arr1[0].deceased },
+          { x: 'REC', text: 'Recovered', y: arr1[0].recovered },
+          { x: 'TST', text: 'Tested', y: arr1[0].tested },];
+      });
+  }
+
 }
